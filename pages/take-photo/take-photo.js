0fe23ea558a66d5cf885fbe17980ee19c5bc7de8
yuanbenhui <yuanbenhui@qutoutiao.net>
2019-04-22 20:11:16 +0800

    update

--- pages/take-photo/take-photo.js
+++ pages/take-photo/take-photo.js
@@ -1,15 +1,16 @@
 //Page Object
 Page({
     data: {
-        
+        flash_flag:"off",
+        flash_path:"http://www.52css.top:8000/images/light_off.png",
     },
-    takePhoto(){
+    takePhoto:function(){
         var ctx = wx.createCameraContext(this);
         ctx.takePhoto({
             quality: 'high',
             success: (res)=>{
                 var src=res.tempImagePath
-                wx.navigateTo({
+                wx.redirectTo({
                     url: '../cropper-image/cropper-image?imgSrc='+src,
                     success: (result) => {
                         console.log(result)
@@ -17,6 +18,24 @@ Page({
                 });
             }
         })
+    },
+    flash_btn:function(){
+        if (this.data.flash_flag=="on"){
+            this.setData({
+                flash_flag:"off",
+                flash_path:"http://www.52css.top:8000/images/light_off.png"
+            })
+            console.log(this.data.flash_flag)
+            console.log(this.data.flash_path)
+        }
+        else{
+            this.setData({
+                flash_flag:"on",
+                flash_path:"http://www.52css.top:8000/images/light_on.png"
+            })
+            console.log(this.data.flash_flag)
+            console.log(this.data.flash_path)
+        }
     }
 });
   
